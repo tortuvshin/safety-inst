00bc3838de26c4a0c05f5d2db37e97ec4735add8
Turtuvshin Byambaa <toroo.byamba@gmail.com>
2017-09-25 14:20:19 +0800

    imei login ui and get imei code

--- app/src/main/java/btgt/mn/safetyinst/LoginImeiActivity.java
+++ app/src/main/java/btgt/mn/safetyinst/LoginImeiActivity.java
@@ -1,13 +1,35 @@
 package btgt.mn.safetyinst;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.AppCompatButton;
+import android.telephony.TelephonyManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class LoginImeiActivity extends AppCompatActivity {
+    private AppCompatButton loginBtn;
+    private EditText passText;
+    private TextView imeiCode;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        imeiCode = (TextView) findViewById(R.id.imei_code);
+        passText = (EditText) findViewById(R.id.password);
+        loginBtn = (AppCompatButton) findViewById(R.id.login);
+
+        TelephonyManager mngr = (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED) {
+
+            return;
+        }
+        imeiCode.setText("Imei: "+mngr.getDeviceId());
     }
 }
