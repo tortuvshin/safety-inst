ce18765bc376d92dfe38435a168775f284192042
Turtuvshin Byambaa <toroo.byamba@gmail.com>
2017-10-02 12:15:04 +0800

    хэрэглэгч нэвтрэх хэсэг

--- app/src/main/java/btgt/mn/safetyinst/LoginImeiActivity.java
+++ app/src/main/java/btgt/mn/safetyinst/LoginImeiActivity.java
@@ -4,6 +4,7 @@ import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.database.Cursor;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -13,12 +14,18 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import btgt.mn.safetyinst.database.UserTable;
 
 public class LoginImeiActivity extends AppCompatActivity {
     private AppCompatButton loginBtn;
     private EditText passText;
     private TextView imeiCode;
-
+    private TextView regLink;
+    UserTable userTable;
+    TelephonyManager mngr;
+    String imei;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,18 +33,96 @@ public class LoginImeiActivity extends AppCompatActivity {
         imeiCode = (TextView) findViewById(R.id.imei_code);
         passText = (EditText) findViewById(R.id.password);
         loginBtn = (AppCompatButton) findViewById(R.id.login);
+        regLink = (TextView) findViewById(R.id.linkReg);
+        userTable = new UserTable(this);
 
-        TelephonyManager mngr = (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
+        mngr = (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED) {
             return;
         }
-        imeiCode.setText("Imei: "+mngr.getDeviceId());
+        imei = mngr.getDeviceId();
+        imeiCode.setText("Imei: " + imei);
 
         loginBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(LoginImeiActivity.this, MainActivity.class));
+                login();
+            }
+        });
+
+        regLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(LoginImeiActivity.this, RegisterActivity.class));
             }
         });
     }
+
+    public void login() {
+        if (!validate()) {
+            Toast.makeText(LoginImeiActivity.this, "Хэрэглэгчийн нэр нууц үг буруу байна", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        loginBtn.setEnabled(false);
+
+        new android.os.Handler().postDelayed(
+                new Runnable() {
+                    public void run() {
+                        Toast.makeText(LoginImeiActivity.this, "Тавтай морилно уу", Toast.LENGTH_SHORT).show();
+                    }
+                }, 3000);
+    }
+
+
+    public boolean validate() {
+        boolean valid = true;
+
+        String password = passText.getText().toString();
+
+
+        if (password.isEmpty() || password.length() < 4) {
+            passText.setError("нууц үг 4-өөс олон тэмдэгт байна");
+            valid = false;
+        } else {
+            passText.setError(null);
+        }
+        if (password.trim().length() > 0) {
+
+            Cursor checkeduser = userTable.checkUser(imei, password);
+            if(checkeduser != null){
+                startManagingCursor(checkeduser);
+                if (checkeduser.getCount() > 0){
+                    stopManagingCursor(checkeduser);
+                    checkeduser.close();
+
+                    new android.os.Handler().postDelayed(
+                            new Runnable() {
+                                public void run() {
+                                    Intent i = new Intent(getApplicationContext(), MainActivity.class);
+                                    i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                                    i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                                    startActivity(i);
+                                }
+                            }, 2000);
+                    valid = true;
+                }
+                else{
+                    valid = false;
+                }
+                stopManagingCursor(checkeduser);
+                checkeduser.close();
+
+            }else{
+                Toast.makeText(getApplicationContext(),
+                        "Database query error",
+                        Toast.LENGTH_SHORT).show();
+                valid = false;
+            }
+        } else {
+            Toast.makeText(LoginImeiActivity.this,"Хэрэглэгчийн нэр нууц үгээ оруулна уу",Toast.LENGTH_SHORT).show();
+            valid = false;
+        }
+        return valid;
+    }
+
 }
