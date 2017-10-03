e5fff9a69d77a03f226d9af1da5dff4bce6c9c33
Turtuvshin Byambaa <toroo.byamba@gmail.com>
2017-10-03 16:08:07 +0800

    snote хүснэгт үүсгэх

--- app/src/main/java/btgt/mn/safetyinst/database/DatabaseHelper.java
+++ app/src/main/java/btgt/mn/safetyinst/database/DatabaseHelper.java
@@ -9,6 +9,8 @@ import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.text.TextUtils;
 
+import btgt.mn.safetyinst.entity.SNote;
+
 public class DatabaseHelper extends SQLiteOpenHelper {
 
     private Context myContext;
@@ -28,6 +30,16 @@ public class DatabaseHelper extends SQLiteOpenHelper {
             UserTable.USER_PROFILE + " TEXT," +
             UserTable.USER_LAST_SIGNED + " TEXT)";
 
+    private static final String CREATE_TABLE_SNOTES = "CREATE TABLE "+ SNoteTable.TABLE_SNOTE+" (" +
+            SNoteTable.SNOTE_ID + " TEXT PRIMARY KEY," +
+            SNoteTable.SNOTE_CAT_ID + " INT," +
+            SNoteTable.SNOTE_NAME + " TEXT," +
+            SNoteTable.SNOTE_ORDER + " TEXT," +
+            SNoteTable.SNOTE_FRAME_TYPE + " INT," +
+            SNoteTable.SNOTE_FRAME_DATA + " TEXT," +
+            SNoteTable.SNOTE_VOICE_DATA + " TEXT," +
+            SNoteTable.SNOTE_TIMEOUT + " INT)";
+
     public DatabaseHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
         myContext = context;
@@ -37,12 +49,13 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     public void onCreate(SQLiteDatabase db)
     {
         db.execSQL(CREATE_TABLE_USERS);
-
+        db.execSQL(CREATE_TABLE_SNOTES);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         dropTable(UserTable.TABLE_USERS);
+        dropTable(SNoteTable.TABLE_SNOTE);
         onCreate(db);
     }
 
