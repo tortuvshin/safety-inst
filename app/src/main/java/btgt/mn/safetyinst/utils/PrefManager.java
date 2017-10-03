7d72aa2c8705fdfc690510fc7c2bd3a488b9fc97
Turtuvshin Byambaa <toroo.byamba@gmail.com>
2017-10-03 17:25:10 +0800

    shared preference manage class

---
+++ app/src/main/java/btgt/mn/safetyinst/utils/PrefManager.java
@@ -0,0 +1,26 @@
+package btgt.mn.safetyinst.utils;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+/**
+ * Created by turtuvshin on 10/3/17.
+ */
+
+
+public class PrefManager {
+    SharedPreferences pref;
+    SharedPreferences.Editor editor;
+    Context _context;
+
+    int PRIVATE_MODE = 0;
+
+    private static final String PREF_NAME = "SafetyInst";
+
+    public PrefManager(Context context) {
+        this._context = context;
+        pref = _context.getSharedPreferences(PREF_NAME, PRIVATE_MODE);
+        editor = pref.edit();
+    }
+
+}
