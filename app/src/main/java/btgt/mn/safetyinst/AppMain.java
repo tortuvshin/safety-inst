511b42cd2fd4a237eb07138278a99816fea15aff
Turtuvshin Byambaa <toroo.byamba@gmail.com>
2017-10-03 13:14:31 +0800

    request permissions #10

---
+++ app/src/main/java/btgt/mn/safetyinst/AppMain.java
@@ -0,0 +1,21 @@
+package btgt.mn.safetyinst;
+
+import android.Manifest;
+import android.app.Application;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.support.v4.content.ContextCompat;
+
+import static android.support.v4.app.ActivityCompat.requestPermissions;
+
+/**
+ * Created by turtuvshin on 10/3/17.
+ */
+
+public class AppMain extends Application{
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+    }
+}
--- app/src/main/java/btgt/mn/safetyinst/MainActivity.java
+++ app/src/main/java/btgt/mn/safetyinst/MainActivity.java
@@ -1,7 +1,9 @@
 package btgt.mn.safetyinst;
 
+import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.content.res.AssetManager;
 import android.graphics.Color;
 import android.net.Uri;
@@ -9,6 +11,7 @@ import android.os.Build;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
@@ -36,6 +39,8 @@ import btgt.mn.safetyinst.fragment.AddInfoFragment;
 import btgt.mn.safetyinst.fragment.FinishFragment;
 import btgt.mn.safetyinst.fragment.SafetyFragment;
 
+import static android.support.v4.app.ActivityCompat.requestPermissions;
+
 public class MainActivity extends AppCompatActivity {
     private static final int NUM_PAGES = 3;
 
@@ -55,7 +60,7 @@ public class MainActivity extends AppCompatActivity {
         }
 
         setContentView(R.layout.activity_main);
-
+        reqPermissions();
         viewPager = (ViewPager) findViewById(R.id.pager);
 
         dotsLayout = (LinearLayout) findViewById(R.id.layoutDots);
@@ -181,6 +186,24 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public int getCount() {
             return NUM_PAGES;
+        }
+    }
+
+    public void reqPermissions(){
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (ContextCompat.checkSelfPermission(MainActivity.this,
+                    Manifest.permission.CAMERA)
+                    != PackageManager.PERMISSION_GRANTED) {
+                requestPermissions(new String[]{Manifest.permission.CAMERA,
+                        Manifest.permission.READ_EXTERNAL_STORAGE,
+                        Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                        Manifest.permission.INTERNET,
+                        Manifest.permission.ACCESS_NETWORK_STATE,
+                        Manifest.permission.ACCESS_FINE_LOCATION,
+                        Manifest.permission.ACCESS_COARSE_LOCATION,
+
+                }, 0);
+            }
         }
     }
 }
