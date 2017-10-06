8fa908a44c4312f5ae975eabfda843551bd64864
Turtuvshin Byambaa <toroo.byamba@gmail.com>
2017-10-06 15:42:08 +0800

    bitmap bytearray convert

---
+++ app/src/main/java/btgt/mn/safetyinst/utils/DbBitmap.java
@@ -0,0 +1,22 @@
+package btgt.mn.safetyinst.utils;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+
+import java.io.ByteArrayOutputStream;
+
+/**
+ * Created by turtuvshin on 10/6/17.
+ */
+
+public class DbBitmap {
+    public static byte[] getBytes(Bitmap bitmap) {
+        ByteArrayOutputStream stream = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.PNG, 0, stream);
+        return stream.toByteArray();
+    }
+
+    public static Bitmap getImage(byte[] image) {
+        return BitmapFactory.decodeByteArray(image, 0, image.length);
+    }
+}
