f678e40fc8a83339d1bded497036839052c1d3f8
Turtuvshin Byambaa <toroo.byamba@gmail.com>
2017-10-06 13:47:50 +0800

    гарын үсэг хадгалсан

--- app/src/main/java/btgt/mn/safetyinst/AddInfoActivity.java
+++ app/src/main/java/btgt/mn/safetyinst/AddInfoActivity.java
@@ -1,12 +1,19 @@
 package btgt.mn.safetyinst;
 
 import android.content.Intent;
+import android.gesture.GestureOverlayView;
 import android.graphics.Bitmap;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileOutputStream;
 
 import btgt.mn.safetyinst.database.SNoteTable;
 import btgt.mn.safetyinst.entity.SNote;
@@ -21,7 +28,38 @@ public class AddInfoActivity extends AppCompatActivity {
         setContentView(R.layout.activity_add_info);
 
         imageView = (ImageView) findViewById(R.id.imageView1);
-        Button photoButton = (Button) findViewById(R.id.button1);
+        Button saveBtn = (Button) findViewById(R.id.save);
+        Button clearBtn = (Button) findViewById(R.id.clear);
+
+        clearBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        saveBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try {
+                    GestureOverlayView gestureView = (GestureOverlayView) findViewById(R.id.signaturePad);
+                    gestureView.setDrawingCacheEnabled(true);
+                    Bitmap bm = Bitmap.createBitmap(gestureView.getDrawingCache());
+                    File f = new File(Environment.getExternalStorageDirectory()
+                                   + File.separator + "signature.png");
+                    f.createNewFile();
+                    FileOutputStream os = new FileOutputStream(f);
+                    os = new FileOutputStream(f);
+                    //compress to specified format (PNG), quality - which is ignored for PNG, and out stream
+                    bm.compress(Bitmap.CompressFormat.PNG, 100, os);
+                    os.close();
+                    Toast.makeText(AddInfoActivity.this, "Амжилттай хадгаллаа", Toast.LENGTH_LONG).show();
+                 } catch (Exception e) {
+                    Log.v("Gestures", e.getMessage());
+                    e.printStackTrace();
+                 }
+            }
+        });
 
         imageView.setOnClickListener(new View.OnClickListener() {
             @Override
